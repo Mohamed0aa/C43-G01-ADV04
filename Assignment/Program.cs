a37e42874d6157e632f677c2c90cffda4ead21c8
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-02-09 20:05:36 +0200

    q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1
+            //Queue<int> queue = new Queue<int>();
+
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+            //queue.Enqueue(4);
+
+            //Stack<int> helper = new Stack<int>();
+            //foreach (int i in queue)
+            //{
+            //    helper.Push(i);
+            //}
+            //foreach (int i in helper)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            
+            #endregion
+
+            
+        }
+    }
+}
